b813e28631cb4533a95adc04bfa1b11f0caeb3a6
mostafasadeghifar <msadeghifar1996@gmail.com>
2023-05-24 11:15:20 +0430

    refactor main.py for added new config

--- process/main.py
+++ process/main.py
@@ -3,6 +3,11 @@
 import requests
 import git
 from git import Repo
+from random import randrange, uniform
+import os
+
+
+
 
 file1 = "https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Sub1.txt"
 file2 = "https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Sub2.txt"
@@ -12,7 +17,7 @@ r1 = requests.get(file1) # create HTTP response object
 r2 = requests.get(file2)
 # send a HTTP request to the server and save
 # the HTTP response in a response object called r
-with open("../file1.txt",'wb') as f:
+with open("../file12.txt",'wb') as f:
   
     # Saving received content as a png file in
     # binary format
@@ -21,7 +26,7 @@ with open("../file1.txt",'wb') as f:
     # to a new file in binary mode.
     f.write(r1.content)
 
-with open("../file2.txt",'wb') as t:
+with open("../file22.txt",'wb') as t:
   
     # Saving received content as a png file in
     # binary format
@@ -30,13 +35,17 @@ with open("../file2.txt",'wb') as t:
     # to a new file in binary mode.
     t.write(r2.content)
 
-repo = git.Repo("/home/masi/Desktop/v2ray-config")
-file1 = 'file1.txt'
-file2 = 'file2.txt'
+f = open("../file22.txt", "a")
+f.write(str(randrange(0, 10))+"\n")
+# f.write(os.path.abspath(".."))
+f.close()
+repo = git.Repo(os.path.abspath(".."))
+file12 = 'file12.txt'
+file22 = 'file22.txt'
 
-repo.index.add([file1 , file2])
+repo.index.add([file12 , file22])
 # repo.index.add([file2])
-repo.index.commit('added new config')
+repo.index.commit('added new config22')
 print('commit successfully')
 
 origin = repo.remote(name='origin')
