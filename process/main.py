a715c831b5429990039582563c70ac5f143ba42d
mostafasadeghifar <msadeghifar1996@gmail.com>
2023-05-24 13:39:26 +0430

    refactor main.py

--- process/main.py
+++ process/main.py
@@ -10,14 +10,14 @@ import os
 
 
 file1 = "https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Sub1.txt"
-file2 = "https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Sub2.txt"
+# file2 = "https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Sub2.txt"
   
 # URL of the file to be downloaded is defined as file
 r1 = requests.get(file1) # create HTTP response object
-r2 = requests.get(file2)
+# r2 = requests.get(file2)
 # send a HTTP request to the server and save
 # the HTTP response in a response object called r
-with open("../file12.txt",'wb') as f:
+with open("../config.txt",'wb') as f:
   
     # Saving received content as a png file in
     # binary format
@@ -26,26 +26,26 @@ with open("../file12.txt",'wb') as f:
     # to a new file in binary mode.
     f.write(r1.content)
 
-with open("../file22.txt",'wb') as t:
+# with open("../file22.txt",'wb') as t:
   
-    # Saving received content as a png file in
-    # binary format
+#     # Saving received content as a png file in
+#     # binary format
   
-    # write the contents of the response (r.content)
-    # to a new file in binary mode.
-    t.write(r2.content)
+#     # write the contents of the response (r.content)
+#     # to a new file in binary mode.
+#     t.write(r2.content)
 
-f = open("../file22.txt", "a")
-f.write(str(randrange(0, 10))+"\n")
-# f.write(os.path.abspath(".."))
-f.close()
+# f = open("../file22.txt", "a")
+# f.write(str(randrange(0, 10))+"\n")
+# # f.write(os.path.abspath(".."))
+# f.close()
 repo = git.Repo(os.path.abspath(".."))
-file12 = 'file12.txt'
-file22 = 'file22.txt'
+config = 'config.txt'
+# file22 = 'file22.txt'
 
-repo.index.add([file12 , file22])
+repo.index.add([config])
 # repo.index.add([file2])
-repo.index.commit('addedsdf new config22')
+repo.index.commit('added new config22')
 print('commit successfully')
 
 origin = repo.remote(name='origin')
